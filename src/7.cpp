736f1b595db47156c25168ac88c58ee0cd1d667b
lauriajule880 <lauriajule880@gmail.com>
2025-03-12 12:30:58 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,16 @@
+
+// Project01.cpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Alice";
+    int age = 30;
+
+    cout << "Hello, my name is " << name << " and I am " << age << " years old." << endl;
+
+    return 0;
+}
