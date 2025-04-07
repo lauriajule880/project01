3dcea8e2bb5b26775d29b318aea4001be7159d1e
lauriajule880 <lauriajule880@gmail.com>
2025-04-07 16:42:24 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    float c = 7.8f;
+    string d = "Hello, World!";
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+
+    return 0;
+}
