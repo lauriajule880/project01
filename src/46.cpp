f8748095977f6c117e3a4260b85e457e79f32701
lauriajule880 <lauriajule880@gmail.com>
2025-05-27 15:15:41 +0000

    Add 46.cpp

---
+++ src/46.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 5;
+    double pi = 3.14159265358979323846;
+    string str1 = "Hello, World!";
+    float f1 = 3.14159265358979323846;
+    int b = 20;
+
+    return 0;
+}
